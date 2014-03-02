cf4eec45a4bcdd3ee164e55217ed06534408e03f
= <=>
2014-03-02 19:15:26 +0800

    只显示系统当前所有的用户姓名.进行计数.

--- showUsers.sh
+++ showUsers.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-#
+#	显示所有用户的用户名.然后列出用户总数.
+
+echo Your system is :  `uname`	#uname 输出所用内核.
+echo Users list:
+allusers=`cat /etc/passwd | awk -F':' '{print $1}'| tr '\n' '     '`
+#	awk 用 : 分割,然后打印第一行,最后用tr进行替换.
+echo ${allusers}
+count=`wc -l /etc/passwd`
+#	使用wc -l 函数进行函数行的统计.
+echo Users count: ${count}
